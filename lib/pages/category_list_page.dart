e27b819326dd2a1fc2f60f7468eda9d969866d9b
amir3482arab <amirarab3482@gmail.com>
2020-11-29 13:34:40 +0330

    Add page category_list_page.dart

---
+++ lib/pages/category_list_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CategoryListPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => CategoryListPageState();
+}
+
+class CategoryListPageState extends State<CategoryListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
